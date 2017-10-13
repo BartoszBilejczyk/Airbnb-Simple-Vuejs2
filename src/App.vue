615307c78fec51149247d23f651e4ab3c98e4342
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-10-13 15:28:26 +0200

    Removed stylus temporarily, added core scss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-
+    <v-container>Hello world</v-container>
   </div>
 </template>
 
@@ -10,6 +10,17 @@ export default {
 }
 </script>
 
-<style lang="stylus">
-  @import './stylus/main'
+<style lang="scss">
+
+@import './assets/styles/main.scss';
+
+html, body {
+  font-family: 'Montserrat', sans-serif !important;
+  box-sizing: border-box;
+}
+
+p, span {
+  font-family: 'Roboto', sans-serif !important;
+}
+
 </style>
